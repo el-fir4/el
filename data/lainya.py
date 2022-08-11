e1ea5771d35fa43a98d30f43cb8a6f69b36309c0
el-fir4 <110964805+el-fir4@users.noreply.github.com>
2022-08-11 08:30:12 +0700

    Update lainya.py

--- data/lainya.py
+++ data/lainya.py
@@ -168,8 +168,8 @@ class Xnxx:
                     self.data_ua.update({pu:x.replace('[#AAAAAA]','')})
                 except KeyboardInterrupt:
                     break
-            open('data/ua.txt','a').write(x+'\n')
-            prints(Panel("berhasil merandom user agent"));input(f" [ {O}Kembali{N} ] ");tt.Brute()
+        open('data/ua.txt','a').write(self.data_ua)
+        prints(Panel("berhasil merandom user agent"));input(f" [ {O}Kembali{N} ] ");tt.Brute()
 
     def dump_flw(self):
         try:
